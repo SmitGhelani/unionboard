98a0cfeee95b125728b1634d286c80b9850f42bb
Smit Ghelani <ghelanismit4@gmail.com>
2024-11-13 23:17:46 +0530

    file upload issue

--- api/index.js
+++ api/index.js
@@ -35,6 +35,14 @@ app.use(fileUpload({
   tempFileDir: './tmp/'
 }))
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+
 app.use(cors({
   origin: 'https://unionboard.smitghelani.xyz',
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
