4f5badfadabcd598237e6bd93c4f0268f429ea38
Smit Ghelani <ghelanismit4@gmail.com>
2024-11-13 22:44:47 +0530

    file upload issue

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload({
   useTempFiles: true,
-  tempFileDir: './tmp/'
+  tempFileDir: `${process.cwd()}/tmp/`
 }))
 
 app.use(cors({
